bf7eb4d8401069366f516bf863c3d971102de8f4
112310207 <146065412+112310207@users.noreply.github.com>
2023-10-19 23:06:48 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,17 +1,31 @@
 #include <stdio.h>
   main(){
     int s1, d1, s2, d2, s3, d3;
-    int time_point[24];
+    int time_point[24]={0};
     scanf("%d%d%d%d%d%d",&s1, &d1, &s2, &d2, &s3, &d3);
 
     for (int i= 0; i < 24; i++) {
-    if (i>=s1 && i<=d1);
-    if (i>=s2 && i<=d2);
-    if (i>=s3 && i<=d3);
-    int min_buses = 0;
+    if (i>=s1 && i<d1) {
+    time_point[i]++;
+    }
+    if (i>=s2 && i<d2) {
+    time_point[i]++;
+    }
+    if (i>=s3 && i<d3){
+    time_point[i]++;
+    }
+    }
+    int min_buses=0 ;
     for (int i = 0; i < 24; i++) {
-    if (time_point[i] > 0);
+    if (time_point[i] > 0){
+    min_buses++;
+    }
+
+    }
     printf("%d\n", min_buses);
     }
-    }
-}
+
+
+
+
+
