9d7eab75ad81513b99f391e3db2b3d3c4ef04928
112310207 <146065412+112310207@users.noreply.github.com>
2023-10-23 19:59:12 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+    int main(){
+    int num1, num2, a1, a2, a3, a4, b1, b2, b3, b4,numplus1,numplus2;
+    scanf("%d%d", &num1, &num2);
+    a1 = num1 / 1000;
+    a2 = (num1 % 1000) / 100;
+    a3 = (num1 % 100) / 10;
+    a4 = (num1 % 10);
+    b1 = num2 / 1000;
+    b2 = (num2 % 1000) / 100;
+    b3 = (num2 % 100) / 10;
+    b4 = (num2 % 10);
+    numplus1 = a1 + a2 + a3 + a4;
+    numplus2 = b1 + b2 + b3 + b4;
+    if (numplus1 > numplus2) printf("%d", num2);
+    else if (numplus1 < numplus2) printf("%d", num1);
+    else {
+          if (num1 < num2) printf("%d", num1);
+          else printf("%d", num2);
+    }
+}
 
